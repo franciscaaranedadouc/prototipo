eb5d7a22d57b10065ddf46e4b57c3e8d0098b8cd
franciscaaranedadouc <fr.aranedar@duocuc.cl>
2025-05-02 22:38:16 -0400

    prueba
    Co-Authored-By: franciscaaranedadouc <186115708+franciscaaranedadouc@users.noreply.github.com>

---
+++ prototipo/prototipo/urls.py
@@ -0,0 +1,50 @@
+"""
+URL configuration for prototipo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from .views import juegos,registro1,registro2,registrotrabajador,sesion,cerrar_sesion,contraseña,perfil,carrito, perfil, VideoGameListAPI
+from django.contrib.auth.decorators import login_required
+from . import views 
+from django.contrib.auth import views as auth_views
+from .views import VideoGameListAPI
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/videogames/', VideoGameListAPI.as_view(), name='videogame-list'),
+    path('juegos', juegos, name='juegos'),
+    path('registro/registro1', registro1, name='registro1'),
+    path('registro/registro2', registro2, name='registro2'),
+    path('registrotrabajador', registrotrabajador, name='registrotrabajador'),
+    path('sesion', sesion, name='sesion'),
+    path('logout', cerrar_sesion, name='logout'),
+    path('registro/contraseña', contraseña, name='contraseña'),
+    path('carrito/carrito', carrito, name='carrito'),
+    path('perfil/perfil', perfil, name='perfil'),
+    path('perfil/', login_required(views.perfil_usuario), name='perfil_usuario'),
+    path('perfil/editar/', login_required(views.editar_perfil), name='editar_perfil'),
+    path('perfil/eliminar', login_required(views.eliminar_perfil), name='eliminar_perfil'),
+    path('admin/', login_required(views.admin_view), name='admin_view'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('perfil/', views.perfil_usuario, name='perfil'),
+    path('perfil/registrocliente1.html', registro1, name='registrocliente1'),
+    path('perfil/registrocliente2.html', registro2, name='registrocliente2'),
+    path('api/videogames/', VideoGameListAPI.as_view(), name='videogame-list'),
+]
+
